2e159c9f5bbbc9b2b0b6c56561563973f13dcee6
imarjunn <singh.arjun535@gmail.com>
2020-11-06 22:47:13 +0530

    Vowel Count

---
+++ VowelCount7Kyu.js
@@ -0,0 +1,14 @@
+function getCount(str) {
+  let vowelsCount = 0;
+  let vowels = ['a','e','i','o','u'];
+  
+  for(let i = 0;i< str.length;i++){
+    if(vowels.includes(str[i]))
+    {
+      vowelsCount++;
+    }
+  }
+  
+  
+  return vowelsCount;
+}
